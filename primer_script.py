1ac7dc88de1f6b8242c984d05a706f746094e6fb
cristianp75466-bit <cristianperez75466@gmail.com>
2026-03-01 10:54:52 -0300

    script que obtiene ip de un dominio

--- primer_script.py
+++ primer_script.py
@@ -1,1 +1,9 @@
-print("proyecto iciando")
+import socket
+objetivo = input("ingresa una pagina (ej:google.com):")
+
+try:
+    ip = socket.gethostbyname(objetivo)
+    print("la ip de", objetivo, "es:", ip)
+except:
+    print("no se pudo obtener ls ip")
+    
